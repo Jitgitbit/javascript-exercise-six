97aa991b1ef7fd74fdc42145c519d79c8f030cda
Jitgitbit <rowowolf@gmail.com>
2020-04-27 09:48:26 +0200

    moveFrog fn done

--- app.js
+++ app.js
@@ -11,4 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentIndex = 76
   let currentTime = 20
   let timerId
+
+  //move the Frog
+  function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch(e.keyCode) {
+      case 37:
+        if(currentIndex % width !== 0) currentIndex -= 1
+        break
+      case 38:
+        if(currentIndex - width >= 0) currentIndex -= width
+        break
+      case 39:
+        if(currentIndex % width < width - 1) currentIndex += 1
+        break
+      case 40:
+        if (currentIndex + width < width * width) currentIndex += width
+        break
+    }
+    squares[currentIndex].classList.add('frog')
+    lose()
+    win()
+  }
 });
